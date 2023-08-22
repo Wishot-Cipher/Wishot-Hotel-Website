ee7df1015f6df9b847f6cc360a29eace37314e82
Alom Wisdom <ogwisdom27@gmail.com>
2023-08-22 04:28:58 +0100

    adjusted a little toggling error

--- src/components/AgeNavbar.jsx
+++ src/components/AgeNavbar.jsx
@@ -133,7 +133,7 @@ export const AgeNavbar = () => {
             {!loggedIn && (
               <button
                 className=" text-md font-medium text-white bg-indigo-600 hover:bg-indigo-700 py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={showMenu}
+                onClick={handleBookButt}
               >
                 <NavLink to={"/login"}>Sign In</NavLink>
               </button>
